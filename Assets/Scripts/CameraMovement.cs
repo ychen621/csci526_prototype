35d745e7adc3c3a8164a3be72020de74b36be87b
ychen621 <ychen621@usc.edu>
2023-09-18 12:41:02 -0700

    Prototype_outline

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float movingSpeed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        mainCamera.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
+    }
+}
