30e0cb07884846e042a5540cdb0213f5b6dc60c1
Jack <jiancong@usc.edu>
2023-09-25 20:18:24 -0700

    Revert "Revert "22 seconds version, auto stop""
    This reverts commit 6a42e260bf73efb8c78b619feb72959474b6100a.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,22 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
-//using UnityEditor.UIElements;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     private Camera mainCamera;
     private float movingSpeed = 1.0f;
-    
-    // Start is called before the first frame update
+    private float elapsedTime = 0f;
+    public float gameDuration = 35f;
+
     void Start()
     {
         mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        mainCamera.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime < gameDuration)
+        {
+            mainCamera.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
+        }
     }
 }
